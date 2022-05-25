53c0d5dc9c97850251b775f50af78f15ecc3ffc3
Ivan Okhotnikov <ivan.okhotnikov@outlook.com>
2022-05-25 15:21:46 +0100

    Progress bars, expanders added

--- forecasting/run_forecaster.py
+++ forecasting/run_forecaster.py
@@ -5,64 +5,80 @@ from utils.plotters import Plotter
 
 st.set_page_config(layout='wide',
                    page_title='Forecasting',
-                   page_icon='fav.png')
+                   page_icon='images/fav.png')
 
 
 def main():
     st.header('Forecasting test data')
-    df = DataReader.get_processed_data_from_gcs(raw=False)
-    if st.button('Plot heatmap of features'):
+    raw = st.checkbox('Read raw data', value=False)
+    df = DataReader.get_processed_data_from_gcs(raw=raw)
+    with st.expander('Plot heatmap of features'):
         st.plotly_chart(Plotter.plot_heatmap(df, FORECAST_FEATURES,
                                              show=False),
                         use_container_width=True)
     uploaded_file = st.file_uploader('Upload raw data file', type=['csv'])
-    window = int(
-        st.number_input('Window size of moving average, seconds',
-                        value=3600,
-                        min_value=1,
-                        max_value=7200,
-                        step=1))
     if uploaded_file is not None:
-        new_df = DataReader.read_newcoming_data(uploaded_file)
-        new_df = Preprocessor.remove_step_zero(new_df)
-        new_df = Preprocessor.feature_engineering(new_df)
-        st.write('Forecast on the new data')
-        for feature in FORECAST_FEATURES:
-            scaler = ModelReader.read_model_from_gcs(f'RNN_{feature}_scaler')
-            forecaster = ModelReader.read_model_from_gcs(f'RNN_{feature}')
-            scaled_new_data = scaler.transform(new_df[feature].values.reshape(
-                -1, 1))
-            sequenced_scaled_new_data = Preprocessor.create_sequences(
-                scaled_new_data, lookback=TIME_STEPS, inference=True)
-            forecast = forecaster.predict(sequenced_scaled_new_data)
-            forecast = scaler.inverse_transform(forecast)
-            st.plotly_chart(Plotter.plot_forecast(df,
-                                                  forecast,
-                                                  feature,
-                                                  new=new_df,
-                                                  plot_ma_all=True,
-                                                  window=window,
-                                                  show=False),
-                            use_container_width=True)
+        with st.expander('Show new forecast'):
+            window = int(
+                st.number_input('Window size of moving average, seconds',
+                                value=3600,
+                                min_value=1,
+                                max_value=7200,
+                                step=1))
+            new_df = DataReader.read_newcoming_data(uploaded_file)
+            new_df = Preprocessor.remove_step_zero(new_df)
+            new_df = Preprocessor.feature_engineering(new_df)
+            st.write('Forecast on the new data')
+            forecast_bar = st.progress(0)
+            for feature in FORECAST_FEATURES:
+                forecast_bar.progress(i / len(FORECAST_FEATURES))
+                scaler = ModelReader.read_model_from_gcs(
+                    f'RNN_{feature}_scaler')
+                forecaster = ModelReader.read_model_from_gcs(f'RNN_{feature}')
+                scaled_new_data = scaler.transform(
+                    new_df[feature].values.reshape(-1, 1))
+                sequenced_scaled_new_data = Preprocessor.create_sequences(
+                    scaled_new_data, lookback=TIME_STEPS, inference=True)
+                forecast = forecaster.predict(sequenced_scaled_new_data)
+                forecast = scaler.inverse_transform(forecast)
+                st.plotly_chart(Plotter.plot_forecast(df,
+                                                      forecast,
+                                                      feature,
+                                                      new=new_df,
+                                                      plot_ma_all=True,
+                                                      window=window,
+                                                      show=False),
+                                use_container_width=True)
     else:
-        st.write('Current forecast')
-        for feature in FORECAST_FEATURES:
-            scaler = ModelReader.read_model_from_gcs(f'RNN_{feature}_scaler')
-            forecaster = ModelReader.read_model_from_gcs(f'RNN_{feature}')
-            scaled_data = scaler.transform(
-                df.iloc[-window:][feature].values.reshape(-1, 1))
-            sequenced_scaled_data = Preprocessor.create_sequences(
-                scaled_data, lookback=TIME_STEPS, inference=True)
-            current_forecast = forecaster.predict(sequenced_scaled_data)
-            current_forecast = scaler.inverse_transform(current_forecast)
-            st.plotly_chart(Plotter.plot_forecast(df,
-                                                  current_forecast,
-                                                  feature,
-                                                  new=None,
-                                                  plot_ma_all=True,
-                                                  window=window,
-                                                  show=False),
-                            use_container_width=True)
+        with st.expander('Show current forecast'):
+            plot_each_unit = st.checkbox('Plot each unit', value=False)
+            window = int(
+                st.number_input('Window size of moving average, seconds',
+                                value=3600,
+                                min_value=1,
+                                max_value=7200,
+                                step=1))
+            forecast_bar = st.progress(0)
+            for i, feature in enumerate(FORECAST_FEATURES):
+                forecast_bar.progress(i / len(FORECAST_FEATURES))
+                scaler = ModelReader.read_model_from_gcs(
+                    f'RNN_{feature}_scaler')
+                forecaster = ModelReader.read_model_from_gcs(f'RNN_{feature}')
+                scaled_data = scaler.transform(
+                    df.iloc[-window:][feature].values.reshape(-1, 1))
+                sequenced_scaled_data = Preprocessor.create_sequences(
+                    scaled_data, lookback=TIME_STEPS, inference=True)
+                current_forecast = forecaster.predict(sequenced_scaled_data)
+                current_forecast = scaler.inverse_transform(current_forecast)
+                st.plotly_chart(Plotter.plot_forecast(df,
+                                                      current_forecast,
+                                                      feature,
+                                                      new=None,
+                                                      plot_ma_all=True,
+                                                      window=window,
+                                                      plot_each_unit=plot_each_unit,
+                                                      show=False),
+                                use_container_width=True)
 
 
 if __name__ == '__main__':
